c6c2ca0a8e7c84d80c4ba536888536a48f006973
pawarrenu <116841783+pawarrenu@users.noreply.github.com>
2024-01-18 19:35:34 +0530

    Create sg-rules.tf

---
+++ terraform/sg-rules.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "web_sg_for_elb" {
+  name   = "web-sg_for_elb"
+  vpc_id = aws_vpc.test_vpc.id
+
+  ingress {
+    description      = "Allow http request from anywhere"
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  ingress {
+    description      = "Allow https request from anywhere"
+    protocol         = "tcp"
+    from_port        = 443
+    to_port          = 443
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Allow https request from anywhere"
+    protocol         = "tcp"
+    from_port        = 22
+    to_port          = 22
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "web_sg_for_ec2" {
+  name   = "web-sg_for_ec2"
+  vpc_id = aws_vpc.test_vpc.id
+
+  ingress {
+    description     = "Allow http request from Load Balancer"
+    protocol        = "tcp"
+    from_port       = 80 # range of
+    to_port         = 80 # port numbersa
+    security_groups = [aws_security_group.web_sg_for_elb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
