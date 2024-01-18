da92d3158ba580d6ec590f40d9b4a2692acb2ebf
pawarrenu <116841783+pawarrenu@users.noreply.github.com>
2024-01-18 19:34:42 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region                   = "us-east-1"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+}
